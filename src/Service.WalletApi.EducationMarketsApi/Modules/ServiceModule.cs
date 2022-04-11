c1add9a4a98be2decf808e8191ba9a65b1cc37c0
dmitriy.b <dmitriy.b@smplt.net>
2022-04-11 16:25:57 +0500

    Use new wallet sdk

--- src/Service.WalletApi.EducationMarketsApi/Modules/ServiceModule.cs
+++ src/Service.WalletApi.EducationMarketsApi/Modules/ServiceModule.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.Logging;
 using MyJetWallet.ApiSecurityManager.Autofac;
 using MyJetWallet.Sdk.RestApiTrace;
-using MyJetWallet.Sdk.Service;
 using Service.Core.Client.Services;
 using Service.TutorialMarkets.Client;
 
@@ -12,8 +11,7 @@ namespace Service.WalletApi.EducationMarketsApi.Modules
 	{
 		protected override void Load(ContainerBuilder builder)
 		{
-			// second parameter is null because we do not store api keys yet for wallet api
-			builder.RegisterEncryptionServiceClient(ApplicationEnvironment.AppName, () => Program.Settings.MyNoSqlWriterUrl);
+			builder.RegisterEncryptionServiceClient();
 
 			builder.RegisterTutorialMarketsClient(Program.Settings.EducationMarketsServiceUrl, Program.LoggerFactory.CreateLogger(typeof (TutorialMarketsClientFactory)));
 
